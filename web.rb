7b93c832dd3bb09d409ba05b975f7dbd953be60d
EC2 Default User <ec2-user@ip-172-31-15-155.ap-northeast-1.compute.internal>
2020-06-17 08:47:56 +0000

    sample form & using rds

--- web.rb
+++ web.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'mysql2'
 
 register Sinatra::Reloader
 
@@ -11,4 +12,38 @@ end
 
 get '/goodbye' do
   erb :goodbye
+end
+
+
+get '/users' do
+
+  host = 'ap-northeast-1.rds.amazonaws.com'
+  username = 'username'
+  password = 'password'
+  database = 'database'
+
+  db_client = Mysql2::Client.new(host: host, username: username, database: database, password: password)
+  result = db_client.query("SELECT COUNT(*) FROM users")
+  @users_count = result.to_a[0]['COUNT(*)']
+  erb :users
+
+end
+
+post '/register' do
+
+  host = 'ap-northeast-1.rds.amazonaws.com'
+  username = 'username'
+  password = 'password'
+  database = 'database'
+
+  client = Mysql2::Client.new(host: host, username: username, database: database, password: password)
+
+  name = params[:name]
+  email = params[:email]
+  birthday = params[:birthday]
+  gender = params[:gender]
+
+  client.query("INSERT INTO users (name, email, birthday, gender) VALUES ('#{name}', '#{email}', '#{birthday}', #{gender})")
+
+  redirect "/users"
 end
